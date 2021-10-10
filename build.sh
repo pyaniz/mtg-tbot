294f5d037670a3f4f59b472d82b147dcad0ce847
Pablo Yaniz <pyaniz@gmail.com>
2021-10-10 15:12:39 -0500

    Added /r to look for rules and legalities when sending the card

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #/bin/bash
-docker build -t mtg-bot .
+#docker build -t mtg-bot .
 docker stop MTG-Bot
 docker rm MTG-Bot
-docker run -d --name MTG-Bot mtg-bot
+docker pull pyaniz/mtg-tbot
+docker run -d --name MTG-Bot pyaniz/mtg-tbot
